45b7d0fb2c6f4a09ff177838acbaa07e66497958
brnbp <bruno9pereira@gmail.com>
2018-07-24 00:08:24 -0300

    minor adjusts

--- consumer.php
+++ consumer.php
@@ -8,11 +8,13 @@ $consumerTopic = $consumer->newTopic("test");
 
 $partition = 0;
 $timeout = 1000;
+
 // The second argument is the offset at which to start consumption. Valid values
 // are: RD_KAFKA_OFFSET_BEGINNING, RD_KAFKA_OFFSET_END, RD_KAFKA_OFFSET_STORED.
 $consumerTopic->consumeStart($partition, RD_KAFKA_OFFSET_BEGINNING);
 
 while (true) {
+
     $msg = $consumerTopic->consume($partition, $timeout);
     
     if ($msg->err) {
